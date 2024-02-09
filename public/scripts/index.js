c43cd5a8fe5b705058bbf2874baeb778ef3a7853
Sanele Madondo <madondo.sal@gmail.com>
2024-02-09 19:54:44 +0200

    Mobile Nav added and fully responsive
    On branch master
     Changes to be committed:
    	modified:   public/scripts/index.js
    	modified:   public/stylesheets/main.css
    	modified:   public/stylesheets/main.css.map
    	modified:   src/sass/abstracts/_utilities.scss
    	modified:   src/sass/layouts/_navbar.scss
    	modified:   src/views/partials/navigation.handlebars

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,32 @@
+//Navbar componets
 const body = document.querySelector('body');
 const shopLink = document.querySelector(".shop-link");
 const catalogueLinks = shopLink.querySelector('div');
 
+const mobileNav = document.querySelector('.mobile-nav');
+const closeNavButton = document.querySelector('.fa-times');
+const openNavButton = document.querySelector(".fa-bars");
+
+//Event Listeners
 shopLink.addEventListener('click', function() {
     catalogueLinks.classList.toggle("shown-catalogue");
 });
 
+closeNavButton.addEventListener("click", function() {
+    mobileNav.classList.remove("shown-nav");
+});
+
+openNavButton.addEventListener("click", function() {
+    mobileNav.classList.add("shown-nav");
+});
+
+
 body.addEventListener('click', function(event) {
     if (!shopLink.contains(event.target)) {
         catalogueLinks.classList.remove("shown-catalogue");
+    }
+
+    if (!document.querySelector('nav').contains(event.target)) {
+        mobileNav.classList.remove("shown-nav");
     }
 })
